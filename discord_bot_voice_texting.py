ff530e6f9d65278ce2149eae27e6521af31ce2d5
MarufSwe <khanmaruf.swe@gmail.com>
2023-02-06 21:51:23 +0600

    voice based texting on discord by bot user

--- discord_bot_voice_texting.py
+++ discord_bot_voice_texting.py
@@ -4,6 +4,7 @@ import pywhatkit
 import datetime
 import wikipedia
 import discord
+import os
 
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
@@ -13,46 +14,51 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[10].id)
 
+
 # convert my voice to virtual voice
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-#voice command here (speak)
+
+# voice command here (speak)
 def take_command():
     try:
         with sr.Microphone() as source:
             print('listening...')
-            voice = listener.listen(source)  #listener listen the user-voice by source
-            user_command = listener.recognize_google(voice)  #by google-api, listener send audio & google back it like text
-            print('user_command: ',user_command)
-            # command = command.lower()
-            # if 'alexa' in command:
-            #     command = command.replace('alexa', '')
-            #     # print(command)
-            #     print('user_command', command)
+            voice = listener.listen(source)  # listener listen the user-voice by source
+            user_command = listener.recognize_google(
+                voice)  # by google-api, listener send audio & google back it like text
+            print('user_command: ', user_command)
     except:
         pass
     return user_command
 
+
 def run_assistant():
     command = take_command()
-    print('get_user_command: ',command)
+    print('get_user_command: ', command)
 
-
-    #for Discord BOT
+    # for Discord BOT
     @client.event
     async def on_ready():
         print(f'Logged in as {client.user}')
-        channel = client.get_channel(1069591088677015604) # bot channel id
+        channel = client.get_channel(1069591088677015604)  # bot channel id
+        await channel.send(command)
+        talk(command)
+        print(command)
 
-        if 'hello' in command:
-            talk(command)
-            print('command: ', command)
-            await channel.send(command)
+        # if 'hello' in command:
+        #     talk(command)
+        #     print('command: ', command)
+        #     await channel.send(command)
 
-    client.run("MTA2OTU1ODY0MTk2MDY5Mzg2MQ.GTea6W.h3ld3cMlMAIcvvhXPFehn0pah64M_vB7MkSbIs") # bot Token
+    # print('User Token is: ', client.user.id)
 
-# while True:
-run_assistant()
+    client.run("MTA2OTU1ODY0MTk2MDY5Mzg2MQ.GhKkXN.Y7gNSZwjLUhkPGbAK2VvJAlXFrrx8FkcXA-ySU")  # bot Token
+    # client.run("OTg2NDkzMjEyODMzMjE4NTgx.GtGJAO.OGi6Y_rwqg_7Zb29ZqFfTS_yWvMItL1RHhZsOs")  # My real Token
+    # client.run("token is here", bot=False)
 
+
+while True:
+    run_assistant()
--- main.py
+++ main.py
@@ -9,19 +9,22 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[10].id)
 
+
 # convert my voice to virtual voice
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-#voice command here (speak)
+
+# voice command here (speak)
 def take_command():
     try:
         with sr.Microphone() as source:
             print('listening...')
-            voice = listener.listen(source)  #listener listen the user-voice by source
-            user_command = listener.recognize_google(voice)  #by google-api, listener send audio & google back it like text
-            print('user_command: ',user_command)
+            voice = listener.listen(source)  # listener listen the user-voice by source
+            user_command = listener.recognize_google(
+                voice)  # by google-api, listener send audio & google back it like text
+            print('user_command: ', user_command)
             # command = command.lower()
             # if 'alexa' in command:
             #     command = command.replace('alexa', '')
@@ -31,13 +34,14 @@ def take_command():
         pass
     return user_command
 
+
 def run_assistant():
     command = take_command()
-    print('get_user_command: ',command)
+    print('get_user_command: ', command)
 
     if 'play' in command:
         song = command.replace('play', '')
-        talk('playing'+ song)
+        talk('playing' + song)
         pywhatkit.playonyt(song)
 
     elif 'time' in command:
@@ -47,7 +51,7 @@ def run_assistant():
 
     elif 'who is' in command:
         person = command.replace('who is', '')
-        info = wikipedia.summary(person, 3) # 3 = take 3 lines of data from wikipedia
+        info = wikipedia.summary(person, 3)  # 3 = take 3 lines of data from wikipedia
         print(info)
         talk(info)
 
@@ -63,6 +67,6 @@ def run_assistant():
     else:
         talk('Please say the command again')
 
+
 while True:
     run_assistant()
-
